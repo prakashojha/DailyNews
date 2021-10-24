217d59a3edce31218442b13449d1d756393be6e9
Bindu Ojha <prakashojha2005@gmail.com>
2021-10-25 02:41:16 +1300

    Update news API-Key to facilitate testing

--- DailyNews/DailyNews/Model/DNNetworkModel.swift
+++ DailyNews/DailyNews/Model/DNNetworkModel.swift
@@ -11,5 +11,6 @@ import Foundation
 //Added here Just for testing purpose
 
 struct DNNetworkModel{
-    static var API_KEY = "bS41P8egr5hoMjY2rxWAHUvXYOW3JML2Q8GIW6FqXu0"
+    //static var API_KEY = "bS41P8egr5hoMjY2rxWAHUvXYOW3JML2Q8GIW6FqXu0"
+    static var API_KEY = "X_Va_UTNKGah38LKlt_Gy3T3KdGnvxmy1tsG7Tp0SNw"
 }
