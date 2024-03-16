d26233b6d191ae093e0d4c0b958f4fc4efa5c269
BPO <prakashojha@users.noreply.github.com>
2024-03-17 05:21:47 +1300

    Update MockURLProtocol.swift

--- DailyNews/DailyNewsTests/Mock/MockURLProtocol.swift
+++ DailyNews/DailyNewsTests/Mock/MockURLProtocol.swift
@@ -18,11 +18,7 @@ class MockURLProtocol: URLProtocol{
     override class func canInit(with request: URLRequest) -> Bool {
         return true
     }
-    
-    override class func canInit(with task: URLSessionTask) -> Bool {
-        return true
-    }
-    
+
     override class func canonicalRequest(for request: URLRequest) -> URLRequest {
         return request
     }
